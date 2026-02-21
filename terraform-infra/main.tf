b04c993af40534bf7f834f3a69a097dcce04964b
ashish28simon <ashish.simon28@gmail.com>
2026-02-22 01:44:49 +0530

    Refactor Google Cloud Storage bucket resource

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,14 +1,4 @@
-resource "google_storage_bucket" "terraform_state" {
-  name          = "terraform-state-${var.env}-${var.project_id}"
+provider "google" {
   project = var.project_id
-  location      = var.region
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
-}
-
-output "bucket_name" {
-  value = google_storage_bucket.terraform_state.name
+  region  = var.region
 }
