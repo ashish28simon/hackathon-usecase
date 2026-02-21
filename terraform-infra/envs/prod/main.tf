30683c4e2321be106fe32e7e35127badb336ba03
ashish28simon <ashish.simon28@gmail.com>
2026-02-22 01:38:16 +0530

    Add project_id to multiple Terraform modules

--- terraform-infra/envs/prod/main.tf
+++ terraform-infra/envs/prod/main.tf
@@ -2,6 +2,7 @@
 module "network" {
   source               = "../../modules/network"
   vpc_name             = var.vpc_name
+  project_id           = var.project_id
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   region               = var.region
@@ -11,6 +12,7 @@ module "network" {
 # IAM Module
 module "iam" {
   source             = "../../modules/iam"
+  project_id           = var.project_id
   env                = var.env
   microservice_names = var.microservice_names
 }
@@ -26,6 +28,7 @@ module "storage" {
 # GKE Cluster Module
 module "gke" {
   source          = "../../modules/gke"
+  project_id      = var.project_id
   env             = var.env
   region          = var.region
   vpc_id          = module.network.vpc_id
@@ -35,6 +38,8 @@ module "gke" {
 # Artifact Registry Module (Docker images)
 module "artifact_registry" {
   source = "../../modules/artifact_registry"
+  project_id = var.project_id
+  repository_id = "prod-docker-repo"
   env    = var.env
   region = var.region
 }
