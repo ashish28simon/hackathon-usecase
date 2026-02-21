e6ba07215b87bd084519ee68f7fb958ac05e4115
ashish28simon <ashish.simon28@gmail.com>
2026-02-22 01:44:15 +0530

    Add Google Storage bucket for Terraform state
    Added Google Cloud Storage bucket resource for Terraform state management with versioning enabled.

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,4 +1,14 @@
-provider "google" {
+resource "google_storage_bucket" "terraform_state" {
+  name          = "terraform-state-${var.env}-${var.project_id}"
   project = var.project_id
-  region  = var.region
+  location      = var.region
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.terraform_state.name
 }
