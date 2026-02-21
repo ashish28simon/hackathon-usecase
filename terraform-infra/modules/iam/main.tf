e0d39d4d9941a944c456f40a3a32c61d65ca332e
ashish28simon <ashish.simon28@gmail.com>
2026-02-22 01:42:24 +0530

    Refactor IAM service account configurations
    Updated service account configuration to include project ID and simplified display name.

--- terraform-infra/modules/iam/main.tf
+++ terraform-infra/modules/iam/main.tf
@@ -1,9 +1,11 @@
 resource "google_service_account" "terraform" {
-  account_id   = "terraform-sa-${var.env}"
-  display_name = "Terraform Service Account for ${var.env}"
+  project = var.project_id
+  account_id   = "terraform-${var.env}"
+  display_name = "Terraform Service Account"
 }
 
 resource "google_service_account" "microservice" {
+  project = var.project_id
   count        = length(var.microservice_names)
   account_id   = "${var.microservice_names[count.index]}-sa-${var.env}"
   display_name = "Service Account for ${var.microservice_names[count.index]} in ${var.env}"
