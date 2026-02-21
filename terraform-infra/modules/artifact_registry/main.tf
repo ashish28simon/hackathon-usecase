55d3d60ed34efb0c04668bada237696a31d9afc2
ashish28simon <ashish.simon28@gmail.com>
2026-02-21 22:48:55 +0530

    Refactor artifact registry repository configuration

--- terraform-infra/modules/artifact_registry/main.tf
+++ terraform-infra/modules/artifact_registry/main.tf
@@ -1,10 +1,6 @@
 resource "google_artifact_registry_repository" "docker_repo" {
-  name        = "microservices-repo-${var.env}"
-  format      = "DOCKER"
-  location    = var.region
-  description = "Docker repo for microservices images"
-}
-
-output "repo_name" {
-  value = google_artifact_registry_repository.docker_repo.name
+  location      = var.region
+  repository_id = var.repository_id
+  description   = "Docker repo for microservices"
+  format        = "DOCKER"
 }
